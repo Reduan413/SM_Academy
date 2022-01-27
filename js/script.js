8354ad318b83f9c8579e74a22329a6e2a1c4b328
reduan <reduan15-11413@diu.edu.bd>
2022-01-27 23:34:21 +0600

    add account

--- js/script.js
+++ js/script.js
@@ -1,8 +1,33 @@
-let navbar = document.querySelector('.header .navbar')
+let navbar = document.querySelector(".header .navbar");
 
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.add('active');
-  }
-  document.querySelector('#close-navbar').onclick = () =>{
-    navbar.classList.remove('active');
-  }
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.add("active");
+};
+document.querySelector("#close-navbar").onclick = () => {
+  navbar.classList.remove("active");
+};
+
+let registerBtn = document.querySelector(".accout-form .register-btn");
+let loginBtn = document.querySelector(".accout-form .login-btn");
+
+registerBtn.onclick = () => {
+  registerBtn.classList.add("active");
+  loginBtn.classList.remove("active");
+  document.querySelector(".accout-form .login-form").classList.remove("active");
+  document.querySelector(".accout-form .register-form").classList.add("active");
+};
+loginBtn.onclick = () => {
+  loginBtn.classList.add("active");
+  registerBtn.classList.remove("active");
+  document.querySelector(".accout-form .login-form").classList.add("active");
+  document.querySelector(".accout-form .register-form").classList.remove("active");
+};
+
+let accountForm = document.querySelector(".accout-form");
+
+document.querySelector("#account-btn").onclick = () => {
+  accountForm.classList.add("active");
+};
+document.querySelector("#close-form").onclick = () => {
+  accountForm.classList.remove("active");
+};
